f92b52dd57b279ed301092d3d4384dcab93da34c
ivadelina <kapranova.milla@yandex.ru>
2022-05-27 18:14:50 +0300

    main

---
+++ src/checkCorrection.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-bitwise */
+/* eslint-disable no-cond-assign */
+/* eslint-disable no-plusplus */
+/* eslint-disable linebreak-style */
+/* eslint-disable eqeqeq */
+/* eslint-disable linebreak-style */
+import { number, btn, validateBox } from './app';
+
+function moonAlgorithm(setValue) {
+  let ch = 0; // счетчик суммы
+  const num = String(setValue).replace(/\D/g, ''); // убираем не числа
+  const isOdd = num.length % 2 !== 0; // проверяем четность
+
+  if (num === '') return false; // возвращаем false, если нет номера карты
+
+  for (let i = 0; i < num.length; i++) {
+    let n = parseInt(num[i], 10); // делаем из строки обратно число для каждого элемента
+
+    ch += (isOdd | 0) === (i % 2) && (n *= 2) > 9 ? (n - 9) : n;
+  }
+  return (ch % 10) === 0;
+}
+
+function checkCorrection(event) {
+  event.preventDefault();
+  validateBox.textContent = '';
+  validateBox.classList.remove('red');
+  validateBox.classList.remove('green');
+  if (moonAlgorithm(number.value)) {
+    validateBox.textContent = 'Проверка номера карты пройдена успешно';
+    validateBox.classList.add('green');
+  } else {
+    validateBox.textContent = 'Некорректный номер карты';
+    validateBox.classList.add('red');
+  }
+}
+
+btn.addEventListener('click', checkCorrection);
---
+++ src/checkSystem.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable eqeqeq */
+
+import {
+  number, btn, mir, visa, mastercart, allcarts, resultBox,
+} from './app';
+
+function checkPaySystem(event) {
+  event.preventDefault();
+  for (const el of allcarts) {
+    el.classList.remove('hide');
+  }
+  resultBox.textContent = '';
+  if (number.value[0] == 2) {
+    visa.classList.add('hide');
+    mastercart.classList.add('hide');
+    resultBox.append('МИР');
+  } else if (number.value[0] == 4) {
+    mir.classList.add('hide');
+    mastercart.classList.add('hide');
+    resultBox.append('VISA');
+  } else if (/^51|52|53|54|55/.test(number.value)) {
+    mir.classList.add('hide');
+    visa.classList.add('hide');
+    resultBox.append('MasterCard');
+  }
+}
+
+btn.addEventListener('click', checkPaySystem);
