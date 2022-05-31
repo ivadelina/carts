0cb12c2017c82a3d70d55caef1d3df85b4f29caa
ivadelina <kapranova.milla@yandex.ru>
2022-05-31 16:58:03 +0300

    main

--- src/__tests__/e2e.test.js
+++ src/__tests__/e2e.test.js
@@ -1,7 +1,7 @@
 import puppetteer from 'puppeteer';
 import { fork } from 'child_process';
 
-jest.setTimeout(30000); // default puppeteer timeout
+jest.setTimeout(50000);
 
 describe('Credit Card Validator form', () => {
   let browser = null;
@@ -41,9 +41,37 @@ describe('Credit Card Validator form', () => {
     const submit = await form.$('[data-id = card_number]');
     submit.click();
     const result = await page.evaluate(() => {
-      let title = document.querySelector('.paySystemResult').textContent;
-      return title
-    });  
+      const title = document.querySelector('.paySystemResult').textContent;
+      return title;
+    });
     expect(result).toBe('MasterCard');
+
+    test('check the card number is correct', async () => {
+      await page.goto(baseUrl);
+      const form1 = await page.$('[data-id = id-form]');
+      const input1 = await form.$('[data-id = card_number]');
+      await input1.type('2221003413074827');
+      const submit1 = await form1.$('[data-id = card_number]');
+      submit1.click();
+      const result1 = await page.evaluate(() => {
+        const title1 = document.querySelector('.validateBox').textContent;
+        return title1;
+      });
+      expect(result1).toBe('Проверка номера карты пройдена успешно');
+    });
+
+    test('check the card number is incorrect', async () => {
+      await page.goto(baseUrl);
+      const form2 = await page.$('[data-id = id-form]');
+      const input2 = await form.$('[data-id = card_number]');
+      await input2.type('2221003413074827');
+      const submit2 = await form2.$('[data-id = card_number]');
+      submit2.click();
+      const result2 = await page.evaluate(() => {
+        const title2 = document.querySelector('.validateBox').textContent;
+        return title2;
+      });
+      expect(result2).toBe('Некорректный номер карты');
+    });
   });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,6 @@ module.exports = {
       {
         test: /\.(gif|png|jpe?g|svg)$/i,
         use: [
-          'file-loader',
           {
             loader: 'image-webpack-loader',
             options: {
